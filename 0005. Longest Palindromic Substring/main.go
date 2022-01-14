b4ac3c654207102b7db43a6d39f9c1c96eb7b5db
Skurhse Rage 🌆🌃🌌 <hello@drruruu.dev>
2022-01-13 23:40:53 -0600

    fmt

--- 0005. Longest Palindromic Substring/main.go
+++ 0005. Longest Palindromic Substring/main.go
@@ -12,26 +12,26 @@ func main() {
 func longestPalindrome(s string) (z string) {
 	z = s[0:1]
 	for i := 0; i < len(s); i++ {
-    indices := [2][2]int{{0,1},{0,0}}
-    for n := 0; n < len(indices); n++ {
-      h, l := indices[n][0], indices[n][1]
-      for {
-        j, k := i-h, i+l
-        if j < 0 || k >= len(s) {
-          break
-        }
-        a, b := s[j], s[k]
-        if a != b {
-          break
-        }
-        q := s[j : k+1]
-        if len(q) > len(z) {
-          z = q
-        }
-        h++
-        l++
-      }
-    }
+		indices := [2][2]int{{0, 1}, {0, 0}}
+		for n := 0; n < len(indices); n++ {
+			h, l := indices[n][0], indices[n][1]
+			for {
+				j, k := i-h, i+l
+				if j < 0 || k >= len(s) {
+					break
+				}
+				a, b := s[j], s[k]
+				if a != b {
+					break
+				}
+				q := s[j : k+1]
+				if len(q) > len(z) {
+					z = q
+				}
+				h++
+				l++
+			}
+		}
 	}
 	return
 }
