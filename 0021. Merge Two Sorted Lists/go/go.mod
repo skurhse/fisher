ce0fb0ca7fbc9465d735b83a8eb8b2ac0b166f69
Skurhse Rage 🌆🌃🌌 <srage@its4logistics.com>
2022-07-06 16:57:44 -0500

    Draft #21

---
+++ 0021. Merge Two Sorted Lists/go/go.mod
@@ -0,0 +1,3 @@
+module merge-two-sorted-lists
+
+go 1.18
---
+++ 0021. Merge Two Sorted Lists/go/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	fmt.Println("Hello, world.")
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val > list2.Val {
+		list1, list2 = list2, list1
+	}
+
+	list1.Next = mergeTwoLists(list1.Next, list2)
+	return list1
+}
