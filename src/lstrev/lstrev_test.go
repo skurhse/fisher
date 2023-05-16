6f20494f22eaa58091dd1a7278040435c0139287
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-05-15 20:29:55 -0500

    works

--- src/lstrev/lstrev_test.go
+++ src/lstrev/lstrev_test.go
@@ -1,9 +1,12 @@
 package lstrev_test
 
 import (
+	"fmt"
+	"math/rand"
 	"reflect"
 	"runtime"
 	"testing"
+	"time"
 
 	. "github.com/skurhse/gresto/lstrev"
 )
@@ -13,6 +16,8 @@ type assert struct {
 	want  []int
 }
 
+var funcs []func(*ListNode) *ListNode = []func(*ListNode) *ListNode{ReverseListRec, ReverseListItr}
+
 func TestReverseLists(t *testing.T) {
 	var data = []assert{
 		{[]int{1, 2, 3}, []int{3, 2, 1}},
@@ -20,16 +25,33 @@ func TestReverseLists(t *testing.T) {
 		{[]int{}, []int{}},
 	}
 
-	funcs := []func(*ListNode) *ListNode{ReverseListRec, ReverseListItr}
-
 	for _, dtm := range data {
 		for _, fnc := range funcs {
 			input, want := NewList(dtm.input), NewList(dtm.want)
 
-			if got := fnc(input); !reflect.DeepEqual(got, want) {
-				fname := runtime.FuncForPC(reflect.ValueOf(fnc).Pointer()).Name()
-				t.Errorf("%s(%v) = %v, want %v", fname, input.ToSlice(), got.ToSlice(), want.ToSlice())
-			}
+			fname := runtime.FuncForPC(reflect.ValueOf(fnc).Pointer()).Name()
+			testname := fmt.Sprintf("%s(%v)", fname, input.ToSlice())
+
+			t.Run(testname, func(t *testing.T) {
+				if got := fnc(input); !reflect.DeepEqual(got, want) {
+					t.Errorf("got %v, want %v", got.ToSlice(), want.ToSlice())
+				}
+			})
 		}
+	}
+}
+
+func BenchmarkReverseLists(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	list := NewList(rand.Perm(1000000))
+
+	for _, fnc := range funcs {
+		fname := runtime.FuncForPC(reflect.ValueOf(fnc).Pointer()).Name()
+
+		b.Run(fname, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fnc(list)
+			}
+		})
 	}
 }
